86cb51a9aecd9a9ec1b73a810b6688aba612b19d
stohn <maik@stohn.de>
2012-02-07 23:36:06 +0100

    changed min temp for extrusion to 180 deg

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -366,7 +366,7 @@ const bool Z_ENDSTOPS_INVERTING = true; // set to true to invert the logic of th
 //this prevents dangerous Extruder moves, i.e. if the temperature is under the limit
 //can be software-disabled for whatever purposes by
 #define PREVENT_DANGEROUS_EXTRUDE
-#define EXTRUDE_MINTEMP 190
+#define EXTRUDE_MINTEMP 180
 #define EXTRUDE_MAXLENGTH (X_MAX_LENGTH+Y_MAX_LENGTH) //prevent extrusion of very large distances.
 
 const int dropsegments=5; //everything with less than this number of steps will be ignored as move and joined with the next movement
