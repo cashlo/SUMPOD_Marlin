63b7f1ced35893e08b90e37f8757ea98b602e8bf
stohn <maik@stohn.de>
2012-02-21 18:42:41 +0100

    lowered the temperature check interval at startup

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -47,7 +47,7 @@
 #define TEMP_SENSOR_BED 1
 
 // Actual temperature must be close to target for this long before M109 returns success
-#define TEMP_RESIDENCY_TIME 30  // (seconds)
+#define TEMP_RESIDENCY_TIME 10  // (seconds)
 #define TEMP_HYSTERESIS 3       // (C°) range of +/- temperatures considered "close" to the target one
 
 // The minimal temperature defines the temperature below which the heater will not be enabled It is used
